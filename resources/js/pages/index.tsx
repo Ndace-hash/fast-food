5ae82862728161b56377c52aca4ab1c53f877d37
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2025-04-03 16:41:53 +0100

    removed a few sections

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -1,32 +1,20 @@
 import { AppAboutSection } from '@/components/app/about-section';
-import { AppBookTableSection } from '@/components/app/book-table-section';
-import { AppChefsSection } from '@/components/app/chefs-section';
 import { AppContactSection } from '@/components/app/contact-section';
-import { AppEventSection } from '@/components/app/event-section';
 import { AppFooterSection } from '@/components/app/footer-section';
-import { AppGallerySection } from '@/components/app/gallery-section';
 import { AppHeader } from '@/components/app/header-section';
 import { AppHeroSection } from '@/components/app/hero-section';
 import { AppMenuSection } from '@/components/app/menu-section';
-import { AppStatsSection } from '@/components/app/stats-section';
 import { AppTestimonialSection } from '@/components/app/testimonial-section';
 import { AppWhyUsSection } from '@/components/app/why-us-section';
 import { type SharedData } from '@/types';
+import { IMeal } from '@/types/meal';
 import { Head, usePage } from '@inertiajs/react';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
-export default function Welcome() {
+export default function Welcome({ meals }: { meals: IMeal[] }) {
     const { auth } = usePage<SharedData>().props;
 
-    const preloader = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        if (preloader.current) {
-            window.addEventListener('load', () => {
-                preloader.current!.remove();
-            });
-        }
-    }, []);
+    console.log(meals);
     useEffect(() => {
         /**
          * Correct scrolling position upon page load for URLs containing hash links.
@@ -56,18 +44,17 @@ export default function Welcome() {
                 <AppHeroSection />
                 <AppAboutSection />
                 <AppWhyUsSection />
-                <AppStatsSection />
-                <AppMenuSection />
+                {/* <AppStatsSection /> */}
+                <AppMenuSection meals={meals} />
                 <AppTestimonialSection />
-                <AppEventSection />
-                <AppChefsSection />
-                <AppBookTableSection />
-                <AppGallerySection />
+                {/* <AppEventSection /> */}
+                {/* <AppChefsSection /> */}
+                {/* <AppBookTableSection /> */}
+                {/* <AppGallerySection /> */}
                 <AppContactSection />
             </main>
 
             <AppFooterSection />
-            <div id="preloader" ref={preloader}></div>
         </>
     );
 }
