c533647a991477cd769f7a74b962acf9ba54be7d
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2025-04-03 16:46:32 +0100

    removed hand coded menu items

---
+++ resources/js/components/menu/item.tsx
@@ -0,0 +1,35 @@
+import { useCartContext } from '@/contexts/CartContext';
+import { IMeal } from '@/types/meal';
+import { FC } from 'react';
+// interface MenuItemProps {
+//     name: string;
+//     price: number;
+//     image_url: string;
+// }
+
+export const MenuItem: FC<{ item: IMeal }> = (props) => {
+    const { item } = props;
+    const { cart, setCart } = useCartContext();
+    return (
+        <div className="col-lg-4 menu-item">
+            <a href={item.image_url} className="glightbox">
+                <img src={item.image_url} className="menu-img img-fluid" alt={`Photo of ${item.name}`} />
+            </a>
+            <h4 className="capitalize">{item.name}</h4>
+            <p className="ingredients">Lorem, deren, trataro, filede, nerada</p>
+            <p className="price">&#8358;{item.price}</p>
+
+            <button
+                className="rounded-2xl bg-red-600 px-2 py-1 font-semibold text-white uppercase"
+                onClick={() => {
+                    const updatedCart = [...cart, item];
+                    localStorage.setItem('cart', JSON.stringify(updatedCart));
+                    setCart(updatedCart);
+                }}
+            >
+                <i className="bi bi-cart-plus me-2"></i>
+                Add To cart
+            </button>
+        </div>
+    );
+};
