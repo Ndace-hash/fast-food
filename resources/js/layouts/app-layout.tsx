0fcb330e864f041531b0e5e0ae1290345516b961
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2025-04-03 16:44:14 +0100

    handling preloder logic

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,14 +1,26 @@
 import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { type BreadcrumbItem } from '@/types';
-import { type ReactNode } from 'react';
+import { useEffect, useRef, type ReactNode } from 'react';
 
 interface AppLayoutProps {
     children: ReactNode;
     breadcrumbs?: BreadcrumbItem[];
 }
 
-export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
-    <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
-        {children}
-    </AppLayoutTemplate>
-);
+export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => {
+    const preloader = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (preloader.current) {
+            window.addEventListener('load', () => {
+                preloader.current!.remove();
+            });
+        }
+    }, []);
+    return (
+        <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
+            {children}
+            <div id="preloader" ref={preloader}></div>
+        </AppLayoutTemplate>
+    );
+};
